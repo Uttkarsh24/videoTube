4df6957735106bac0ce342e7e7ea06caa220ee29
Uttkarsh24 <uttkarshsingh1973@gmail.com>
2024-12-10 02:40:40 +0530

    user controller setup

--- src/app.js
+++ src/app.js
@@ -14,5 +14,11 @@ app.use(express.urlencoded({extended:true,limit:"16kb"})) //config for url data
 app.use(express.static("public"))
 // app.use(express.cookieParser()) 
 
+//routes import 
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
 
 export { app }
